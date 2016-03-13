586a688e481df842c3040b6c65d2c74bf30028a8
Ed Crump <ed.crump@gmail.com>
2016-03-13 16:25:36 -0700

    Add /api/Arrived/:airport backend server API.

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var path = require('path');
+var FlightAware = require('flightaware.js');
+
+// Get access to the FlightAware 2.0 client API ...
+// See http://flightaware.com/commercial/flightxml for username/apiKey ...
+var username = 'your-flightaware-username';
+var apiKey = 'your-flightaware-apiKey';
+var client = new FlightAware(username, apiKey);
 
 var app = express();
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/Arrived/:airport', function(req, res) {
+    client.Arrived({
+        airport: req.params.airport, 
+    }, function(err, result) { 
+        if (err) return res.send(err);
+        res.json(result);
+    });
 });
 
 var port = 8000;
